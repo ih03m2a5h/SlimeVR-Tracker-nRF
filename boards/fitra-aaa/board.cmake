fe297826810593efd179decce20db72e5490d866
ih03m2a5h <21174940+ih03m2a5h@users.noreply.github.com>
2026-03-21 16:21:01 +0900

    RPDPでの書き込み調整

--- boards/fitra-aaa/board.cmake
+++ boards/fitra-aaa/board.cmake
@@ -4,6 +4,7 @@
 
 if(CONFIG_SOC_NRF54L15_CPUAPP)
 	board_runner_args(jlink "--device=nRF54L15_M33" "--speed=4000")
+	board_runner_args(pyocd "--target=nrf54l")
 elseif (CONFIG_SOC_NRF54L15_CPUFLPR)
   board_runner_args(jlink "--device=nRF54L15_RV32")
 endif()
@@ -14,6 +15,9 @@ endif()
 
 # board_set_debugger_ifnset(jlink)
 # board_set_flasher_ifnset(jlink)
+board_set_debugger_ifnset(pyocd)
+board_set_flasher_ifnset(pyocd)
 
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
